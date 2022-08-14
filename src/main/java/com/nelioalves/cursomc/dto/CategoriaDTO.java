1ffc40176116cde94cc9e063bd9bad8bdd449c2e
T-Pontes <tiagopontes10@gmail.com>
2022-08-13 21:17:41 -0300

    VAlidação sintática com Bean Validation

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;//O pacote javax.validation não vem habilitado por padrão a partir da versão 2.3 do Spring Boot sendo necessário o downloade das dependências
+import javax.validation.constraints.Size;//A anotação @Empty foi substituída pela @Size
+
 import com.nelioalves.cursomc.domain.Categoria;
+
 
 public class CategoriaDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Size(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {	
